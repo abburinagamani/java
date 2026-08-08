eb762add265c1224e95bfde7315f0635d2228848
abburinagamani <abburinagamani430@gmail.com>
2026-08-08 07:51:33 +0530

    updated hypotenuse file

--- src/com/HypotenuseTriangle.java
+++ src/com/HypotenuseTriangle.java
@@ -1,5 +1,21 @@
 package com;
-
+import java.util.*;
 public class HypotenuseTriangle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double a;
+		double b;
+		double c;
+		System.out.print("Enter the value of A: ");
+		a=sc.nextDouble();
+		
+		System.out.print("Enter the value of B: ");
+		b=sc.nextDouble();
+		
+		c=Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+		System.out.println("The hypotenuse of the triangle is: "+c+"cm");
+		
+		sc.close();
+	}
 
 }
