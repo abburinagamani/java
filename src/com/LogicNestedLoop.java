49d67b99be4f4793c56c5759afa6eb138f83b753
abburinagamani <abburinagamani430@gmail.com>
2026-08-09 11:49:16 +0530

    practiced nested loop

---
+++ src/com/LogicNestedLoop.java
@@ -0,0 +1,28 @@
+package com;
+import java.util.*;
+public class LogicNestedLoop {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int rows;
+		int columns;
+		char symbol;
+		System.out.print("Enter the no of rows:");
+		rows=sc.nextInt();
+		
+		System.out.print("Enter the no of columns:");
+		columns=sc.nextInt();
+		
+		System.out.print("Enter the symbol");
+		symbol=sc.next().charAt(0);
+		
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<columns;j++) {
+				System.out.print(symbol);
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
